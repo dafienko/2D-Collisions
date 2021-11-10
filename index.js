54fe9fadf9bac683eb1bf63fb7a733825c42d5a3
Damien <chickenshalom@gmail.com>
2021-11-10 12:47:58 -0500

    i don't remember what I did

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const context = canvas.getContext('2d');
 var W = canvas.width;
 var H = canvas.height;
 
+function rad(d) {
+	return d * (Math.PI / 180)
+}
+
+function deg(r) {
+	return r * (180 / Math.PI)
+}
+
 function circle(x, y, r, color) {
     context.beginPath();
     context.arc(x, y, r, 0, 2 * Math.PI, false);
@@ -11,16 +19,75 @@ function circle(x, y, r, color) {
     context.fill();
 }
 
+function arrow(from, to, color) {
+	var nDir = from.subtract(to, true)
+	nDir.normalize();
+	var arrowLeft = nDir.rotate(rad(-45), false, true).multiply(15, true)
+	arrowLeft.add(to)
+	var arrowRight = nDir.rotate(rad(45), false, true).multiply(15, true)
+	arrowRight.add(to)
+
+	context.beginPath();
+	context.lineWidth = 3
+	context.strokeStyle = color
+	context.moveTo(from.x , from.y )
+	context.lineTo(to.x , to.y)
+	
+	//*
+	context.lineTo(arrowLeft.x, arrowLeft.y)
+	context.moveTo(to.x, to.y)
+	context.lineTo(arrowRight.x, arrowRight.y)
+	//*/
+	context.stroke();
+}
+
+function relToAbs(v) {
+	//return v
+	return new Vec2(v.x * 1.0 * W, v.y * 1.0 * H)
+}
+
 const elasticity = 1
+
+function hexToDec(hex) {
+	var n = 0
+	hex = hex.toUpperCase()
+
+	for (var i = 0; i < hex.length; i++) {
+		var digit = hex.charCodeAt(hex.length - (i + 1));
+		if (digit >= 65) {
+			digit -= 55
+		} else {
+			digit -= 48
+		}
+		n += digit * Math.pow(16, i)
+	}
+
+	return n
+}
+
+function decToHex(n) {
+	var i = 0;
+	var hex = ""
+
+	while (n > 0) {
+		var digit = n % 16
+		n = Math.floor(n / 16)
+		
+		if (digit < 10) {
+			hex = digit.toString() + hex
+		} else {
+			hex = String.fromCharCode(digit + 55) + hex
+		}
+	}
+
+	return hex;
+}
 
 class Ball {
     constructor(mass, color) {
         this.color = color
-        this.x = .5
-        this.y = .5
-
-        this.vx = 0
-        this.vy = 0
+        this.position = new Vec2(.5, .5)
+        this.velocity = new Vec2(0, 0)
 
         this.setMass(mass)
     }
@@ -31,77 +98,73 @@ class Ball {
     }
 
     checkCollision(ball) {
-        var distance = Math.sqrt(Math.pow(this.x -  ball.x, 2) + Math.pow(this.y - ball.y, 2))
+        var distance = ball.position.distance(this.position)
         if (distance < this.radius + ball.radius) {
             var intersectionDistance = (this.radius + ball.radius) - distance
             
-            var dirx = (ball.x - this.x) / distance
-            var diry = (ball.y - this.y) / distance
-            
-            var collisionX = this.x + dirx * (this.radius - intersectionDistance * .5)
-            var collisionY = this.y + diry * (this.radius - intersectionDistance * .5)
+            var normal = this.position.subtract(ball.position, true).normalize(true)
 
-            // make it so the balls aren't intersecting anymore
-            this.x -= intersectionDistance * .5 * dirx
-            this.y -= intersectionDistance * .5 * diry            
-            ball.x += intersectionDistance * .5 * dirx
-            ball.y += intersectionDistance * .5 * diry
+			var collisionPosition = this.position.add(normal.multiply(this.radius - intersectionDistance * .5, true), true)
 
-            var m1X = this.mass * this.vx
-            var m1Y = this.mass * this.vy
-            
-            var m2X = ball.mass * ball.vx
-            var m2Y = ball.mass * ball.vy
-
-            var vxf = (m1X + m2X) / (this.mass + ball.mass)
-            var vyf = (m1Y + m2Y) / (this.mass + ball.mass)
-
-            this.vx = vxf
-            this.vy = vyf
-            ball.vx = vxf
-            ball.vy = vyf
+			// make balls no longer intersect
+			this.position.add(normal.multiply(intersectionDistance * .5, true))
+			ball.position.add(normal.multiply(intersectionDistance * -.5, true))
+			
+			var σ = this.radius + ball.radius
+			this.velocity.subtract(normal.multiply(2 * this.velocity.dot(normal)))
+			normal.multiply(-1)
+			ball.velocity.subtract(normal.multiply(2 * ball.velocity.dot(normal)))
         }
     }
 
     applyVelocity(dt) {
-        this.x += this.vx * dt
-        this.y += this.vy * dt
+        this.position.add(this.velocity.multiply(dt, true))
     }
 
     render() {
-        circle(this.x * W, this.y * H, this.radius * W, this.color)
-
-        context.font = '36px serif';
-        var str = this.mass + " kg"
-        var metrics = context.measureText(str)
-        context.fillStyle = "#FFFFFF"
-        context.strokeStyle = "#000000"
-        context.fillText(str, this.x * W - metrics.width / 2, this.y * H + 12);
-        context.strokeText(str, this.x * W - metrics.width / 2, this.y * H + 12);
+		var r = this.radius * W
+		
+        circle(this.position.x * W, this.position.y * H, r, this.color)
     }
+
+	renderVelocity() {
+		var v = this.velocity.multiply(.1, true)
+		var c = "#"
+		
+		for (var i = 0; i < 3; i++) {
+			var ch = this.color.substring(1 + i * 2, (i + 1) * 2 + 1)
+			var component = decToHex(Math.max(180, hexToDec(ch)))
+			
+			if (component.length < 2) {
+				component = "0" + component
+			}
+			
+			c += component
+		}
+		
+		arrow(relToAbs(this.position), relToAbs(this.position.add(v, true)), c)
+	}
 }
 
 var d1 = .45, d2 = .45
 
-var ball1 = new Ball(1, "#FF0000")
-ball1.vx = Math.random() * 2
-var ball2 = new Ball(3, "#0000FF")
-ball2.vy = -Math.random() * 2
+var ball1 = new Ball(Math.random() * 3, "#FF0000")
+ball1.velocity = new Vec2(Math.random() * 2, 0)
+var ball2 = new Ball(Math.random() * 3, "#0000FF")
+ball2.velocity = new Vec2(0, -Math.random() * 2)
 
-var v1i = "<" + Math.round(ball1.vx * 100) / 100 + ", " + Math.round(ball1.vy * 100) / 100 + ">"
-var v2i = "<" + Math.round(ball2.vx * 100) / 100 + ", " + Math.round(ball2.vy * 100) / 100 + ">"
-var v1 = Math.sqrt(Math.pow(ball1.vx, 2) + Math.pow(ball1.vy, 2))
-var v2 = Math.sqrt(Math.pow(ball2.vx, 2) + Math.pow(ball2.vy, 2))
+var v1i = "<" + Math.round(ball1.velocity.x * 100) / 100 + ", " + Math.round(ball1.velocity.y * 100) / 100 + ">"
+var v2i = "<" + Math.round(ball2.velocity.x * 100) / 100 + ", " + Math.round(ball2.velocity.y * 100) / 100 + ">"
+var v1 = ball1.velocity.length()
+var v2 = ball2.velocity.length()
 
 if (v1 > v2) {
     d2 = (v2 * d1) / v1
 } else {
     d1 = (v1 * d2) / v2
 }
-ball1.x = .5 - d1
-ball1.y = .5
-ball2.x = .5
-ball2.y = .5 + d2
+ball1.position = new Vec2(.5 - d1, .5)
+ball2.position = new Vec2(.5, .5 + d2)
 
 var lastTime = Date.now()
 function draw() {
@@ -114,6 +177,9 @@ function draw() {
 
     W = canvas.width;
     H = canvas.height;
+
+	context.fillStyle = "#F0F0F0";
+	context.fillRect(0, 0, W, H);
     
     ball1.applyVelocity(dt)
     ball2.applyVelocity(dt)
@@ -122,12 +188,23 @@ function draw() {
 
     ball1.render()
     ball2.render()
+	ball1.renderVelocity()
+	ball2.renderVelocity()
 
+	var fontSize = 30
+	context.font = fontSize + 'px serif';
     context.fillStyle = ball1.color
-    context.fillText(v1i, .05 * W, .5 * H);
-    var m = context.measureText(v2i)
+    context.fillText(v1i, fontSize, .5 * H - fontSize * .5);
+    var massStr = Math.round(ball1.mass * 100) / 100 + "kg"
+	context.fillText(massStr, fontSize, .5 * H + fontSize * .5)
+	var m = context.measureText(v2i)
     context.fillStyle = ball2.color
-    context.fillText(v2i, .5 * W - m.width / 2, .95 * H);
+    context.fillText(v2i, .5 * W - m.width / 2, H - fontSize * 2);
+	massStr = Math.round(ball2.mass * 100) / 100 + "kg"
+	m = context.measureText(massStr)
+	context.fillText(massStr, .5 * W - m.width / 2, H - fontSize * 1);
+
+	//arrow(Vec2(.5 * W, .5 * H), Vec2(.7 * W, .6 * H), "#00FF00")
 
     // execute loop function over and over
     requestAnimationFrame(draw);
